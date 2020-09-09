958173b4b3a61835254ed0d5df620fe0384db879
ryan woods <federer5050@gmail.com>
2020-09-09 16:46:32 +0100

    testing app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
+document.querySelector('okes').addEventListener('click', getJokes);
 
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
